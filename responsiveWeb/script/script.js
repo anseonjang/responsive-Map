69bbe7791f54ecb2f967364ec5351dec4cbb5d2e
anseonjang <64743348+anseonjang@users.noreply.github.com>
2023-06-08 12:17:34 +0900

    Add files via upload

---
+++ responsiveWeb/script/script.js
@@ -0,0 +1,11 @@
+var toggleBtn = document.querySelector(".navbar-toggleBtn");
+var menu = document.querySelector(".navbar-menu");
+var icons = document.querySelector(".navbar-icons");
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle("active");
+    icons.classList.toggle("active");
+}
+)
+
+//.toggle():선택한 요소가 보이면 숨기고, 숨겨저있으면 나타냄
